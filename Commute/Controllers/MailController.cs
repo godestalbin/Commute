e86c20c52d8acccc396e81caa95030aaec12a76b
Guy Olivier de Saint Albin <godestalbin@gmail.com>
2012-11-30 09:26:56 +0800

    Test mail

--- Commute/Controllers/MailController.cs
+++ Commute/Controllers/MailController.cs
@@ -5,20 +5,47 @@ using System.Web;
 using System.Web.Mvc;
 using Mvc.Mailer;
 using Commute.Models;
+using System.Net.Mail;
 
 namespace Commute.Controllers
 {
     public class MailController : BaseController
     {
-        //public Mail mail = new Mail();
-        //Mail mail = new Mail();
-        //mail.Welcome().Send();
         //Display mail sent as view - for testing purpose only
         public ActionResult Welcome(int userId)
         {
-            User user = db.User.Find(userId); //Retrieve user
+            //Send mail as test
+            //Mail mail = new Mail();
+            //mail.Welcome().Send();
+            User user = db.User.Find(userId);
             return View(user); //Display /Views/Welcome the view used to generate mail
         }
+
+        //Send manually a mail
+        public ActionResult Send(string to = "godestalbin@leroymerlin.cn", string title = "mail title", string body = "mail body")
+        {
+
+            try
+            {
+                var message = new MailMessage(); //from is taken from the web.config
+                message.Subject = title;
+                message.Body = body;
+                message.To.Add(to);
+
+                var client = new SmtpClient();
+                //client.EnableSsl = true; //moved to Web.config
+                client.Timeout = 900000; //Default time out is 100000 (=100 seconds);
+                client.Send(message);
+                ViewBag.ErrorMessage = "Success: Mail sent to " + message.To[0] + " by " + message.From;
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = "Error: " + ex.Message;
+            }
+
+            return View();
+        }
+
     }
 
     public class Mail : MailerBase
@@ -47,36 +74,3 @@ namespace Commute.Controllers
 
     }
 }
-
-////Send manually a mail
-//public ActionResult Send(string to = "godestalbin@leroymerlin.cn", string title = "mail title", string body = "mail body")
-//{
-
-//    try
-//    {
-//        var message = new MailMessage(); //from is taken from the web.config
-//        message.Subject = "Mail title";
-//        message.Body = "Body - test 4";
-//        message.To.Add(to);
-
-//        var client = new SmtpClient();
-//        client.EnableSsl = true;
-//        client.Timeout = 900000; //Default time out is 100000 (=100 seconds);
-//        client.Send(message);
-//        ViewBag.ErrorMessage = "Success: Mail sent";
-//    }
-//    catch (Exception ex)
-//    {
-//        ViewBag.ErrorMessage = "Error: " + ex.Message;
-//    }
-
-//    return View();
-//}
-
-//public Mail()
-//{
-//    MasterName = "~/Views/Shared/_Mail.cshtml";
-//}
-
-            //ViewBag.Name = "GOdeStAlbin";
-            
