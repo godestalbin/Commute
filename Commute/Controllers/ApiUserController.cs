0e7719776a2f0c0dcf68f06df423912f47c5c1f4
Guy Olivier de Saint Albin <godestalbin@gmail.com>
2013-01-27 14:55:15 +0800

    Google webmaster meta

---
+++ Commute/Controllers/ApiUserController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Commute.Models;
+
+namespace Commute.Controllers
+{
+    public class ApiUserController : ApiController
+    {
+        private Context db = new Context();
+
+        // GET api/ApiUser
+        public IEnumerable<User> GetUsers()
+        {
+            return db.User.AsEnumerable();
+        }
+
+        // GET api/ApiUser/5
+        public User GetUser(int id)
+        {
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return user;
+        }
+
+        // PUT api/ApiUser/5
+        public HttpResponseMessage PutUser(int id, User user)
+        {
+            if (ModelState.IsValid && id == user.Id)
+            {
+                db.Entry(user).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/ApiUser
+        public HttpResponseMessage PostUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, user);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = user.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        //Check user password
+        public HttpResponseMessage CheckUser(String userName, String password)
+        {;
+        HttpResponseMessage response;
+            if ( userName == "godestalbin" && password == "cpc" )
+                response = Request.CreateResponse(HttpStatusCode.OK, true);
+            else
+                response = Request.CreateResponse(HttpStatusCode.Unauthorized, false);
+            return response;
+        }
+
+        // DELETE api/ApiUser/5
+        public HttpResponseMessage DeleteUser(int id)
+        {
+            User user = db.User.Find(id);
+            if (user == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.User.Remove(user);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
